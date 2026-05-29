fcd1a63966696737613f55f88641b1821972c48b
guindastemc-boop <guindastemc@gmail.com>
2026-05-29 09:39:33 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,20 +1,9 @@
 <?php
+session_start();
 
-$host = 'localhost';
-$dbname = 'mc_guindaste';
-$user = 'root';
-$pass = '';
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
-} catch (PDOException $e) {
-    die('Erro ao conectar no banco de dados.');
+if (isset($_SESSION['usuario_id'])) {
+    header('Location: ../app/views/dashboard.php');
+    exit;
 }
+
+require_once '../app/auth/login.php';
